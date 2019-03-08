009308bf4b0e643af0a68b344b8e97ea6e811b66
ZepmanBC <zepman@gmail.com>
2019-03-08 18:54:52 +0100

    question ok

--- papyrobot/tests/test_question.py
+++ papyrobot/tests/test_question.py
@@ -24,36 +24,36 @@ class TestAnswer():
 
     def test_question_1(self):
         query = "Salut GrandPy ! Est-ce que tu connais l'adresse d'OpenClassrooms ?"
-        wanted = "OpenClassrooms"
+        wanted = "OpenClassrooms".lower()
         result = self.question.analyze(query)
         assert result == wanted
 
     def test_question_2(self):
         query = "où se trouve l'Arc de Triomphe?"
-        wanted = "Arc Triomphe"
+        wanted = "Arc Triomphe".lower()
         result = self.question.analyze(query)
         assert result == wanted
 
     def test_question_3(self):
         query = "Quelle est l'adresse de la Tour Eiffel?"
-        wanted = "Tour Eiffel"
+        wanted = "Tour Eiffel".lower()
         result = self.question.analyze(query)
         assert result == wanted
 
     def test_question_4(self):
         query = "Dis Papy, c'est quoi l'adresse de l'Elysée?"
-        wanted = "Elysée"
+        wanted = "Elysée".lower()
         result = self.question.analyze(query)
         assert result == wanted
 
     def test_question_5(self):
         query = "Tu connais l'adresse de l'Opéra Garnier?"
-        wanted = "Opéra Garnier"
+        wanted = "Opéra Garnier".lower()
         result = self.question.analyze(query)
         assert result == wanted
 
     def test_question_null(self):
         query = ""
-        wanted = False
+        wanted = ""
         result = self.question.analyze(query)
         assert result == wanted
--- papyrobot/utils/question.py
+++ papyrobot/utils/question.py
@@ -1,13 +1,42 @@
 #! /usr/bin/env python
+import json
 
 
 class Question():
 
     def __init__(self):
-        pass
+        with open('papyrobot/static/json/stopwords_fr.json') as json_data: 
+            self.stopwords = json.load(json_data)
+        stopword_custom = ['adresse', 'trouve', 'quelle', 'quel', 'papy', 'grandpy', 'connais', 'dis', 'salut']
+        [self.stopwords.append(x) for x in stopword_custom]
+        self.punctuation = [",", ".", "?", "!", ";", "'", "-"]
+
 
     def analyze(self, query):
-        pass
+        analyzed_list = list()
+        query = query.lower()
+        for punc in self.punctuation:
+            query = query.replace(punc, ' ')
+        for word in query.split():
+            if not word in self.stopwords:
+                analyzed_list.append(word)
+        analyzed = ' '.join(analyzed_list)
+        return analyzed
 
 if __name__ == "__main__":
-    pass
+    """
+    Salut GrandPy ! Est-ce que tu connais l'adresse d'OpenClassrooms ?
+    où se trouve l'Arc de Triomphe?
+    Quelle est l'adresse de la Tour Eiffel?
+    Dis Papy, c'est quoi l'adresse de l'Elysée?
+    Tu connais l'adresse de l'Opéra Garnier?
+    """
+    import os
+    os.system('clear')
+    question = Question()
+    query = "Salut GrandPy ! Est-ce que tu connais l'adresse d'OpenClassrooms ?"
+    # query = "où se trouve l'Arc de Triomphe?"
+    # query = "Quelle est l'adresse de la Tour Eiffel?"
+    # query = "Dis Papy, c'est quoi l'adresse de l'Elysée?"
+    # query = "Tu connais l'adresse de l'Opéra Garnier?"
+    print(question.analyze(query))
