c1ad8c2e2df0914f2e676f1431338819f0eaaa59
ZepmanBC <zepman@gmail.com>
2019-03-14 23:59:38 +0100

    mose en place du mode conversation

--- papyrobot/static/js/ajax.js
+++ papyrobot/static/js/ajax.js
@@ -1,18 +1,21 @@
+var mapNumber = 0;
+
 function putDataInTextArea(data){
     document.getElementById('textArea').innerHTML = '';
     if (data['keywords']){
-        var question = "Tu cherches où se trouve " + data['keywords'] + "?";
+        mapNumber++;
+        // var question = "Tu cherches où se trouve " + data['keywords'] + "?";
         var answer = "Bien sur mon poussin, voici l'adresse : " + data['formatted_address'];
         var story = "Mais t'ai-je déjà raconté l'histoire de ce quartier qui m'a vu en culottes courtes ? " + data['story']
         
-        document.getElementById('textArea').innerHTML += '<p>' + question + '</p>';
+        // document.getElementById('textArea').innerHTML += '<p>' + question + '</p>';
         document.getElementById('textArea').innerHTML += '<p>' + answer + '</p>';
-        document.getElementById('textArea').innerHTML += '<div id="map_container"><div id="map"></div></div>'
+        document.getElementById('textArea').innerHTML += '<div class="map_container"><div id="map'+mapNumber+'" class="map"></div></div>'
         document.getElementById('textArea').innerHTML += '<p>' + story + '</p>';
 
         // initMap
         var lat_lng = {lat: data['location'][0], lng: data['location'][1]};
-        var map = new google.maps.Map(document.getElementById('map'), {
+        var map = new google.maps.Map(document.getElementById('map'+mapNumber), {
             center: lat_lng,
             zoom: 14
         });
@@ -20,6 +23,14 @@ function putDataInTextArea(data){
     } else {
         document.getElementById('textArea').innerHTML += "<p>Je n'ai pas bien compris ta question ou alors je ne connais pas ce lieu, essaie encore !</p>";
     }
+    
+}
+
+function putInHistory(speaker, data){
+    if(speaker == "userSpeak"){classText = " text-right";}else{classText="";};
+    dataFormated = '<div class="col-12'+classText+'"><div class="' + speaker + '">' + data + '</div></div>'
+    document.getElementById("history").innerHTML += dataFormated;
+    window.scrollTo(0,document.body.scrollHeight);
 }
 
 const questionInput =  document.getElementsByClassName("question")[0]
@@ -32,13 +43,23 @@ questionInput.addEventListener("keyup", function(event) {
         req.addEventListener("load", function () {
             var data = JSON.parse(req.responseText);
             console.log(req.responseText);
+            papyText = document.getElementById('textArea').innerHTML
             putDataInTextArea(data);
             console.log("loaded...");
             // hide loading div
             document.getElementsByClassName("loading")[0].style.display = 'none';
+            
+            // put previous papy speech (TextArea innerHTML) in history div
+            putInHistory('papySpeak', papyText);
+            // put user speech (input value) in history div
+            putInHistory('userSpeak', questionInput.value);
+            // clear input
             questionInput.value = '';
         });
         req.send(null);
         console.log("loading...");
+        
     }
 });
+
+
--- papyrobot/static/js/front.js
+++ papyrobot/static/js/front.js
@@ -1,15 +1,16 @@
 
 // set good width for the footer
 window.onload = function(event) {
-    headerFooterWidth();
+    resizeAll();
+    // Hide loading page when windows is loaded
     document.getElementsByClassName("loading")[0].style.display = 'none';
 }
 
 window.onresize = function(event) {
-    headerFooterWidth();
+    resizeAll();
 };
 
-function headerFooterWidth(){
+function resizeAll(){
     var mainWidth = document.getElementsByClassName('main-div')[0].offsetWidth;
     // set good width to header and footer
     document.getElementsByTagName('header')[0].style.width = mainWidth + "px";
@@ -18,20 +19,10 @@ function headerFooterWidth(){
     document.getElementById('papy').style.width = (mainWidth/6) + "px";
     var papyHeight = document.getElementById('papy').height
     // set vertical position of virgule-bulle according to papy pic size
-    document.getElementById('virgule-bulle').style.marginTop = (papyHeight / 3) + "px";
+    document.getElementById('virgule-bulle').style.top = (papyHeight * 0.4) + "px";
     // set height for papy pic div
     document.getElementById("papy-div").style.height = papyHeight + 30 + "px";;
     // set loading img in the center
     loadingHeight = document.querySelector(".loading img").height
     document.querySelector(".loading img").style.marginTop = (screen.height / 2 - loadingHeight) + "px";
 }
-
-
-// // Création d'une requête HTTP
-// var req = new XMLHttpRequest();
-// // Requête HTTP GET synchrone vers le fichier langages.txt publié localement
-// req.open("GET", "toto.txt", false);
-// // Envoi de la requête
-// req.send(null);
-// // Affiche la réponse reçue pour la requête
-// console.log(req.responseText);
