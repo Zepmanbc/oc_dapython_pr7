3358fc6ad73ae1d3c74e0c2a03c9e1129216c413
ZepmanBC <zepman@gmail.com>
2019-03-14 21:34:46 +0100

    ajout d'un regex pour papyrobot

--- papyrobot/utils/question.py
+++ papyrobot/utils/question.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 """Question module"""
+import re
 from string import punctuation
 import json
 
@@ -22,7 +23,7 @@ class Question():
         """
         with open('papyrobot/static/json/stopwords_fr.json') as json_data:
             self.stopwords = json.load(json_data)
-        stopword_custom = ['adresse', 'trouve', 'quelle', 'quel', 'papy', 'grandpy', 'connais', 'dis', 'salut']
+        stopword_custom = ['adresse', 'trouve', 'quelle', 'quel', 'connais', 'dis', 'salut']
         self.stopwords += stopword_custom
 
 
@@ -38,18 +39,21 @@ class Question():
         """
         # remove punctuation from query
         query =  query.translate(query.maketrans(punctuation, " " * len(punctuation)))
+        # remove GrandPy, PyBot like words
+        query = re.sub(r"(([a-zA-Z-]*[bB]ot|[a-zA-Z-]*[pP]y))", "", query)
         # remove stopwords
         analyzed = ' '.join([x for x in query.split() if not x.lower() in self.stopwords])
         return analyzed
 
 if __name__ == "__main__":
-    # import os
-    # os.system('clear')
-    # question = Question()
+    import os
+    os.system('clear')
+    question = Question()
+    query = "Salut GrandPy ! PyBot GrandBot Est-ce que tu connais l'adresse d'OpenClassrooms ?"
     # query = "Salut GrandPy ! Est-ce que tu connais l'adresse d'OpenClassrooms ?"
     # query = "où se trouve l'Arc de Triomphe?"
     # query = "Quelle est l'adresse de la Tour Eiffel?"
     # query = "Dis Papy, c'est quoi l'adresse de l'Elysée?"
     # query = "Tu connais l'adresse de l'Opéra Garnier?"
-    # print(question.analyze(query))
+    print(question.analyze(query))
     pass
